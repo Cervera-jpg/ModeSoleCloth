3d2077a422a5ad8ad9b8e432e9c423c14ccc723f
vurtunegodz <carljoseph.diego@tup.edu.ph>
2024-01-21 22:27:08 +0800

    Updated a file
    Added a underline on the nav links

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart from "../Assets/cart_icon.png";
 import { PiPantsFill } from "react-icons/pi";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="main logo" />
       </div>
       <ul className="nav-menu">
-        <li>
-          Home
-          <hr />
+        <li
+          onClick={() => {
+            setMenu("home");
+          }}
+        >
+          Home{menu === "home" ? <hr /> : <></>}
         </li>
-        <li>Women</li>
-        <li>Men</li>
-        <li>Kids</li>
+        <li
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          Women{menu === "women" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          Men{menu === "men" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          Kids{menu === "kids" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
