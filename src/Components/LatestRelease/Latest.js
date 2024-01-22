aeb375e1dc47ace11d225f1e04614a3788ff1eb1
vurtunegodz <carljoseph.diego@tup.edu.ph>
2024-01-22 21:00:15 +0800

    Added a file
    Created a latest release section

---
+++ src/Components/LatestRelease/Latest.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Latest.css";
+const Latest = () => {
+  return (
+    <div className="new-collections">
+      <h1>LATEST RELEASES</h1>
+      <hr />
+    </div>
+  );
+};
+export default Latest;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Poster from "../Components/FrontPoster/Poster";
 import Trending from "../Components/Trending/Trending";
 import Footer from "../Components/Footer/Footer";
+import Latest from "../Components/LatestRelease/Latest";
 
 const Home = () => {
   return (
     <div>
       <Poster />
       <Trending />
+      <Latest />
       <Footer />
     </div>
   );
