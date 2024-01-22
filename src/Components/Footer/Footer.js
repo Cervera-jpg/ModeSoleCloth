0efd1eb8be85cce4bb111356e164efb4603569e7
vurtunegodz <carljoseph.diego@tup.edu.ph>
2024-01-22 20:51:25 +0800

    Updated a file.
    Progress with the footer section

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,29 +9,29 @@ import linkedin_icon from "../Assets/linkedin.png";
 const Footer = () => {
   return (
     <div className="footer">
-      <div className="footer-logo">
-        <img src={logo} alt="" />
-        <div className="footer-text">
-          <p>You Can Contact Us At Our Social Media Accounts</p>
+      <div className="logo">
+        <img src={logo} className="logo" alt="" />
+      </div>
+      <div className="footer-text">
+        <p>You Can Contact Us at Our Social Media Accounts</p>
+      </div>
+      <div className="footer-social-icon">
+        <div className="footer-icons-container">
+          <img src={instagram_icon} className="icon" alt="" />
         </div>
-        <div className="footer-social-icon">
-          <div className="footer-icons-container">
-            <img src={instagram_icon} alt="" />
-          </div>
-          <div className="footer-icons-container">
-            <img src={facebook_icon} alt="" />
-          </div>
-          <div className="footer-icons-container">
-            <img src={github_icon} alt="" />
-          </div>
-          <div className="footer-icons-container">
-            <img src={linkedin_icon} alt="" />
-          </div>
+        <div className="footer-icons-container">
+          <img src={facebook_icon} className="icon" alt="" />
         </div>
-        <div className="footer-copyright">
-          <hr />
-          <p>Copyright 2024 - All Right Reserved.</p>
+        <div className="footer-icons-container">
+          <img src={github_icon} className="icon" alt="" />
         </div>
+        <div className="footer-icons-container">
+          <img src={linkedin_icon} className="icon" alt="" />
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>Copyright 2024 - All Right Reserved.</p>
       </div>
     </div>
   );
