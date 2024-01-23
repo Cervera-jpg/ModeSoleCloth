fa845e50e885f37d6787a6a1b37e079da408c17a
vurtunegodz <patriciamae.ejusa@tup.edu.ph>
2024-01-23 19:42:08 +0800

    Updated a file
    Added products in the trending display.

---
+++ src/Components/Products/Trending_Display.js
@@ -0,0 +1,37 @@
+import p1_img from "../Assets/Kids/kids_6.jpeg";
+import p2_img from "../Assets/Kids/kids_7.jpeg";
+import p3_img from "../Assets/Womens/womens_7.jpeg";
+import p4_img from "../Assets/Womens/womens_3.jpeg";
+
+let Trending_Display = [
+  {
+    id: 1,
+    name: "Yale University Set",
+    image: p1_img,
+    new_price: 46.0,
+    old_price: 157,
+  },
+  {
+    id: 2,
+    name: "Striped Shirt Cool Set",
+    image: p2_img,
+    new_price: 94.0,
+    old_price: 185,
+  },
+  {
+    id: 3,
+    name: "Stylish Skirt Set",
+    image: p3_img,
+    new_price: 94.0,
+    old_price: 185,
+  },
+  {
+    id: 4,
+    name: "Official Clothing Set",
+    image: p4_img,
+    new_price: 69.0,
+    old_price: 115.5,
+  },
+];
+
+export default Trending_Display;
--- src/Components/Trending/Trending.js
+++ src/Components/Trending/Trending.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Trending.css";
+import Display from "../Display/Display";
+import Trending_Display from "../Products/Trending_Display";
 const Trending = () => {
   return (
     <div className="popular">
       <h1>TRENDING FITS</h1>
       <hr />
+      <div className="popular-item">
+        {Trending_Display.map((item, i) => {
+          return (
+            <Display
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
