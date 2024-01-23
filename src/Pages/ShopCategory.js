a7a5e8d035453025b9b8473f9b4bf032488ac923
Cervera-jpg <cerveraarsenio115@gmail.com>
2024-01-23 20:01:24 +0800

    Update shopcategory.js

--- src/Pages/ShopCategory.js
+++ src/Pages/ShopCategory.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./CSS/ShopCategory.css";
-const ShopCategory = () => {
-  return <div></div>;
+import { ShopContext } from "../Context/ShopContext";
+
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext);
+  return (
+    <div className="shop-category">
+      <img src={props.banner} alt="" />
+    </div>
+  );
 };
 
 export default ShopCategory;
