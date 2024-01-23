0c37e5f90926cbad8ddf1536b2e1299964027ca3
vurtunegodz <carljoseph.diego@tup.edu.ph>
2024-01-23 21:06:24 +0800

    Updated a file.
    Added a products to the latest section

--- src/Components/LatestRelease/Latest.js
+++ src/Components/LatestRelease/Latest.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./Latest.css";
+import Display from "../Display/Display";
+import Latest_Display from "../Products/Latest_Display";
 const Latest = () => {
   return (
-    <div className="new-collections">
+    <div className="latest">
       <h1>LATEST RELEASES</h1>
       <hr />
+      <div className="latest_products">
+        {Latest_Display.map((item, i) => {
+          return (
+            <Display
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
