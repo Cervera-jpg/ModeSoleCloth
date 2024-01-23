466bc5589ab0585bd5041cc2010131330db6b8f3
patriciamaeejusa <patriciamae.ejusa@tup.edu.ph>
2024-01-23 23:15:24 +0800

    Updated a file
    working on the css file.

--- src/Components/Advertisement/Advertisement_1.js
+++ src/Components/Advertisement/Advertisement_1.js
@@ -4,15 +4,15 @@ import logo from "../Assets/logo.png";
 import model2 from "../Assets/model_2.png";
 const Advertisement_1 = () => {
   return (
-    <div className="offers">
-      <div className="offers-left">
+    <div className="offers1">
+      <div className="offers-left1">
         <h1>Newest Releases</h1>
         <h2>Just for You</h2>
         <p>What Are You Waiting For?</p>
         <img src={logo} alt="logo"></img>
       </div>
-      <div className="offers-right">
-        <img src={model2} className="model1" alt="" />
+      <div className="offers-right1">
+        <img src={model2} className="model2" alt="" />
       </div>
     </div>
   );
