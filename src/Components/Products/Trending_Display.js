35e87050bfdaf12e73bcf63e0312a7674bc02702
patriciamaeejusa <patriciamae.ejusa@tup.edu.ph>
2024-01-23 23:35:39 +0800

    Update Trending_Display.js

--- src/Components/Products/Trending_Display.js
+++ src/Components/Products/Trending_Display.js
@@ -32,6 +32,34 @@ let Trending_Display = [
     new_price: 8950.0,
     old_price: 12650.0,
   },
+  {
+    id: 1,
+    name: "Yale University Set",
+    image: p1_img,
+    new_price: 5250.0,
+    old_price: 10250.0,
+  },
+  {
+    id: 2,
+    name: "Striped Shirt Cool Set",
+    image: p2_img,
+    new_price: 5950.0,
+    old_price: 9450.0,
+  },
+  {
+    id: 3,
+    name: "Stylish Skirt Set",
+    image: p3_img,
+    new_price: 7550.0,
+    old_price: 13250.0,
+  },
+  {
+    id: 4,
+    name: "Official Clothing Set",
+    image: p4_img,
+    new_price: 8950.0,
+    old_price: 12650.0,
+  },
 ];
 
 export default Trending_Display;
