fd9a0a161953a0dcffe8d788dee380e5c565e37a
patriciamaeejusa <patriciamae.ejusa@tup.edu.ph>
2024-01-23 22:56:48 +0800

    Update Advertisement_1.js

--- src/Components/Advertisement/Advertisement_1.js
+++ src/Components/Advertisement/Advertisement_1.js
@@ -6,6 +6,7 @@ const Advertisement_1 = () => {
       <div className="offers-left">
         <h1>Newest Releases</h1>
         <h2>Just for You</h2>
+        <p>What Are You Waiting For?</p>
       </div>
     </div>
   );
