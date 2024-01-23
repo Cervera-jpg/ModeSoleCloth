28bb38462f7d51b17e66ef46ee90146ef957a83a
patriciamaeejusa <patriciamae.ejusa@tup.edu.ph>
2024-01-23 22:56:12 +0800

    Update Advertisement_1.js

---
+++ src/Components/Advertisement/Advertisement_1.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Advertisement_1 = () => {
+  return (
+    <div className="offers">
+      <div className="offers-left">
+        <h1>Newest Releases</h1>
+        <h2>Just for You</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Advertisement_1;
