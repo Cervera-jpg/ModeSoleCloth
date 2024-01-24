10dfc79f028a35e74b8a780bd3692ee76d281d40
Namichwan <jayemmorada@gmail.com>
2024-01-24 14:35:20 +0800

    Update loginpage

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -7,6 +7,11 @@ const LoginSignup = () => {
     <div className='loginsignup'>
       <div className="loginsignup-container">
         <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
 };
 
 export default LoginPage;
