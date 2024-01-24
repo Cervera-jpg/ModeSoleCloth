9e84b0a68609463f7ab8427a8d02a6354af2132e
Namichwan <jayemmorada@gmail.com>
2024-01-24 14:36:10 +0800

    Update loginpage

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -12,6 +12,11 @@ const LoginSignup = () => {
           <input type="email" placeholder='Email Address' />
           <input type="password" placeholder='Password' />
         </div>
+         <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
 };
 
 export default LoginPage;
