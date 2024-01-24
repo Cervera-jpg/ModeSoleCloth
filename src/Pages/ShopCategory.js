4790c84cabef6ee3c477f40ecd2267655371ffe2
Cervera-jpg <cerveraarsenio115@gmail.com>
2024-01-25 00:17:35 +0800

    Update shopcategory

--- src/Pages/ShopCategory.js
+++ src/Pages/ShopCategory.js
@@ -6,43 +6,43 @@ import Display from "../Components/Display/Display";
 
 const ShopCategory = (props) => {
   const { Product_ID } = useContext(ShopContext);
-  const [visibleProducts, setVisibleProducts] = useState(48); // Initial number of visible products
+  const [visibleProducts, setVisibleProducts] = useState(
+    props.initialVisible || 4
+  ); // Initial number of visible products
 
   const loadMore = () => {
     setVisibleProducts((prevVisibleProducts) => prevVisibleProducts + 4); // Increase the number of visible products by 4
   };
+
+  const filteredProducts = Product_ID.filter(
+    (item) => props.category === item.category
+  );
 
   return (
     <div className="shop-category">
       <img className="shopcategory-banner" src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
-          <span>Showing 1-{visibleProducts}</span> out of {Product_ID.length}{" "}
-          products
+          <span>Showing 1-{visibleProducts}</span> out of{" "}
+          {filteredProducts.length} products
         </p>
         <div className="shopcategory-sort">
           Sort by <img src={dropdown_icon} alt="" />
         </div>
       </div>
       <div className="shopcategory-products">
-        {Product_ID.slice(0, visibleProducts).map((item, i) => {
-          if (props.category === item.category) {
-            return (
-              <Display
-                key={i}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                new_price={item.new_price}
-                old_price={item.old_price}
-              />
-            );
-          } else {
-            return null;
-          }
-        })}
+        {filteredProducts.slice(0, visibleProducts).map((item, i) => (
+          <Display
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
-      {visibleProducts < Product_ID.length && (
+      {visibleProducts < filteredProducts.length && (
         <button className="shopcategory-loadmore" onClick={loadMore}>
           Explore More
         </button>
