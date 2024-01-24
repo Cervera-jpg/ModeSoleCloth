133bb437c0b50df18890f6c2cead7725bc015367
vurtunegodz <carljoseph.diego@tup.edu.ph>
2024-01-24 14:24:40 +0800

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,6 +6,7 @@ import Latest from "../Components/LatestRelease/Latest";
 import Advertisement from "../Components/Advertisement/Advertisement";
 import Advertisement_1 from "../Components/Advertisement/Advertisement_1";
 import Sale from "../Components/Sale/Sale";
+
 const Home = () => {
   return (
     <div>
